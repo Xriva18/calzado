0a9e02f0d7e88b091b0d4652dd10d2f18980e72a
Xriva18 <kevinguanoluisa2004@gmail.com>
2024-07-16 01:04:58 -0500

    Cambios minimos

--- js/navbar.js
+++ js/navbar.js
@@ -11,21 +11,23 @@ $(document).ready(function () {
         // Añade la clase 'active' al enlace sobre el que se hizo clic
         $(this).addClass("active");
     });
-});
 
-$(document).ready(function () {
+
+
+    // Remueve la clase 'active'
     $('.dropdown-item').click(function () {
         // Remueve la clase 'active' de cualquier elemento que la tenga
         $('.nav-link').removeClass('active');
         // Añade la clase 'active' al enlace 'Otros'
         $('.nav-item.dropdown > a.nav-link').addClass('active');
     });
-});
 
-$(document).ready(function () {
-    // Agrega un controlador de eventos 'click' al elemento '.navbar-brand'
     $(".navbar-brand").click(function () {
         // Elimina la clase 'active' de todos los elementos '.nav-item a'
         $(".nav-item a").removeClass("active");
     });
+    $("#inputBuscarNV, #btnBuscarNV").click(function () {
+        $(".nav-item a").removeClass("active");
+    });
 });
+
