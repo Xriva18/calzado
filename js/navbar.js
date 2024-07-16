78308a532093d1c4a394e53d0bf724122c665c8e
Xriva18 <kevinguanoluisa2004@gmail.com>
2024-07-16 00:30:31 -0500

    Arregle formato y modifique el java scritp

--- js/navbar.js
+++ js/navbar.js
@@ -12,3 +12,20 @@ $(document).ready(function () {
         $(this).addClass("active");
     });
 });
+
+$(document).ready(function () {
+    $('.dropdown-item').click(function () {
+        // Remueve la clase 'active' de cualquier elemento que la tenga
+        $('.nav-link').removeClass('active');
+        // Añade la clase 'active' al enlace 'Otros'
+        $('.nav-item.dropdown > a.nav-link').addClass('active');
+    });
+});
+
+$(document).ready(function () {
+    // Agrega un controlador de eventos 'click' al elemento '.navbar-brand'
+    $(".navbar-brand").click(function () {
+        // Elimina la clase 'active' de todos los elementos '.nav-item a'
+        $(".nav-item a").removeClass("active");
+    });
+});
