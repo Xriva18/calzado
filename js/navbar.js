d2a5267a55e6cdf3e2ba8b0ccb1842ff9df796de
Xriva18 <kevinguanoluisa2004@gmail.com>
2024-07-15 23:33:04 -0500

    Nav Bar Arreglo

---
+++ js/navbar.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    // Selecciona todos los enlaces dentro de la lista y escucha el evento click
+    $(".nav-item a").click(function (e) {
+        // Previene el comportamiento predeterminado del enlace
+        e.preventDefault();
+
+        // Elimina la clase 'active' de todos los enlaces
+        $(".nav-item a").removeClass("active");
+
+        // Añade la clase 'active' al enlace sobre el que se hizo clic
+        $(this).addClass("active");
+    });
+});
