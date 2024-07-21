476b81f475670fd53cec5f4f17184136bae2a9c1
Xriva18 <kevinguanoluisa2004@gmail.com>
2024-07-20 22:00:43 -0500

    cards funcional

---
+++ js/cardsDinamcias2.js
@@ -0,0 +1,46 @@
+var zapatillas = [
+    {
+        id: 'Z001', nombre: 'PUMA California Tol Logo', descripcion: 'Vas a estar cómodo dentro y fuera de la cancha con estas zapatillas retro, inspiradas en la cultura pop clásica y en el estilo de baloncesto.',
+        tallas: '39,40,41', precio: '109.99$', imagen: './img/Deportivo/depImg1.jpg'
+    },
+    {
+        id: 'Z002', nombre: 'Nike Air Max 270', descripcion: 'Estas zapatillas ofrecen una comodidad excepcional y un estilo moderno con su unidad Max Air en el talón y su diseño inspirado en los años 90.',
+        tallas: '39,40,41', precio: '149.99$', imagen: './img/Deportivo/depImg2.jpg'
+    },
+    {
+        id: 'Z003', nombre: 'Adidas Ultraboost', descripcion: 'Estas zapatillas de running te brindan una increíble amortiguación y retorno de energía para que puedas correr más rápido y por más tiempo.',
+        tallas: '39,40,41', precio: '169.99$', imagen: './img/Deportivo/depImg3.jpg'
+    },
+];
+$(document).ready(function () {
+    recorrerArray();
+});
+
+function recorrerArray() {
+    $('#cardDeportivo').empty();
+    for (var i = 0; i < zapatillas.length; i++) {
+        newTr =
+            ///otra tarjeta
+            '<div class="col-md-4 my-2">' +
+            '<div class="card shadow border rounded" id="divCard1Crd">' +
+            '<img src="' + zapatillas[i].imagen + '" class="card-img-top" alt="..." id="imgCardImg1Crd">' +
+            '<div class="card-body border" id="divCardBody1Crd" style="height: 300px; background-color: #e3f2fd;">' +
+            '<div id="divCardTitle1Crd" style="height: 8%;">' +
+            '<h5 class="card-title" id="h5CardTitle1Crd">' + zapatillas[i].nombre + '</h5>' +
+            '</div>' +
+            '<div class="card-text mt-4" id="divCardText1Crd" style="height: 60%;">' +
+            '<p id="pCardDesc1Crd" class="text-justify" style="height: 100px;">' + zapatillas[i].descripcion + '</p>' +
+            '<p class="text-right font-weight-bold">' + 'Tallas: ' + '<label class="font-italic font-weight-light">' + zapatillas[i].tallas +
+            '</label>' + '</p >' + '</div >' +
+            '<div class="text-right mt-2" id="divCardAction1Crd">' +
+            '<label class="mr-2" id="lblPrice1Crd">' + zapatillas[i].precio + '</label>' +
+            '<a href="#" class="btn btn-primary" id="aCardLink1Crd">Conoce Más</a>' +
+            '</div >' +
+            '</div >' +
+            '</div >' +
+            '</div >'
+        //xd
+        $('#cardDeportivo').append(newTr);
+    }
+}
+
