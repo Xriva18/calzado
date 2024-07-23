1895ca819d8b839f64c697613a697de68b53510c
Xriva18 <kevinguanoluisa2004@gmail.com>
2024-07-23 18:07:36 -0500

    areglos del nav var y carsdinamicas

--- js/cardsDinamcias2.js
+++ js/cardsDinamcias2.js
@@ -1,279 +1,45 @@
 
 
 $(document).ready(function () {
-    card(zapatillas);
+    card(zapatillas);//onclik en el navbar
 });
 
+var productosGlobales = []; // Variable global para almacenar los productos
+
 function card(productos) {
+    productosGlobales = productos; // Almacena los productos en la variable global
     $('#cardDeportivo').empty();
     $('#cardDeportivoS').hide();
     for (var i = 0; i < productos.length; i++) {
-        var ConoceMas = '<a class="btn btn-primary" onclick="imprimirAlerta(\'' + i + '\');">Conoce Más</a>';
+        var ConoceMas = '<a class="btn btn-primary" onclick="imprimirAlerta(' + i + ');">Conoce Más</a>';
         var newTr = `
-            <div class="col-md-4 my-2">
-            <div class="card shadow border rounded">
-                <img src="${productos[i].imagen1}" class="card-img-top" alt="...">
-                <div class="card-body border" style="height: 300px; background-color: #e3f2fd;">
-                <div style="height: 8%;">
-                    <h5 class="card-title">${productos[i].nombre}</h5>
-                </div>
-                <div class="card-text mt-4" style="height: 60%;">
-                    <p class="text-justify" style="height: 100px;">${productos[i].descripcion}</p>
-                    <div
-                                    class="row  text-right d-flex justify-content-end mr-2 mt-2">
-                                    <p
-                                        class="font-weight-bold mr-3">Tallas:</p><label>
-                                        ${productos[i].talla1},
-                                        ${productos[i].talla2},
-                                        ${productos[i].talla3}</label>
+                <div class="col-md-4 my-2">
+                    <div class="card shadow border rounded">
+                        <img src="${productos[i].imagen1}" class="card-img-top" alt="...">
+                        <div class="card-body border" style="height: 300px; background-color: #e3f2fd;">
+                            <div style="height: 8%;">
+                                <h5 class="card-title">${productos[i].nombre}</h5>
+                            </div>
+                            <div class="card-text mt-4" style="height: 60%;">
+                                <p class="text-justify" style="height: 100px;">${productos[i].descripcion}</p>
+                                <div class="row text-right d-flex justify-content-end mr-2 mt-2">
+                                    <p class="font-weight-bold mr-3">Tallas:</p>
+                                    <label>
+                                        ${productos[i].talla1}, ${productos[i].talla2}, ${productos[i].talla3}
+                                    </label>
                                 </div>
-                    <p class="text-right font-weight-bold">
-                    </label>
-                    </p>
+                                <p class="text-right font-weight-bold"></p>
+                            </div>
+                            <div class="text-right">
+                                <label class="mr-2">${productos[i].precio}</label>
+                                ${ConoceMas}
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="text-right">
-                    <label class="mr-2">${productos[i].precio}</label>
-                    ${ConoceMas}
-                </div>
-                </div>
-            </div>
-            </div>`;
+                <script src="./js/visualizarProducto.js"></script>`;
         $('#cardDeportivo').append(newTr);
     }
 }
 
-function imprimirAlerta(j) {
-    var productos = zapatillas;
-    $('#cardDeportivoS').show();
-    $('#cardDeportivoS').empty();
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-    var newTr2 = `
-        <script src="./assets/js/templatemo.js"></script>
-    <div class="container" style="margin-bottom:80px;">
-            <div class="row m-3"> <!---id-->
-                <div class="col-lg-5 mt-2">
-                    <div class="card mb-3 shadow border rounded">
-                        <img class="card-img img-fluid"
-                            src="${productos[j].imagen1}" alt="Card image cap"
-                            id="product-detail">
-                    </div>
-                    <div class="row">
-                        <div class="col-1 align-self-center">
-                            <a href="#multi-item-example" role="button"
-                                data-bs-slide="prev">
-                                <i class="text-dark fas fa-chevron-left"></i>
-                                <span class="sr-only">Previous</span>
-                            </a>
-                        </div>
-                        <div id="multi-item-example"
-                            class="col-10 carousel slide carousel-multi-item"
-                            data-bs-ride="carousel">
-                            <div class="carousel-inner product-links-wap"
-                                role="listbox">
-                                <div class="carousel-item active">
-                                    <div class="row">
-                                        <div class="col">
-                                            <a href="#">
-                                                <img
-                                                    class="card-img img-fluid shadow border rounded"
-                                                    style="height: 120px;"
-                                                    src="${productos[j].imagen1}"
-                                                    alt="Product Image 1">
-                                            </a>
-                                        </div>
-                                        <div class="col">
-                                            <a href="#">
-                                                <img
-                                                    class="card-img img-fluid shadow border rounded"
-                                                    style="height: 120px;"
-                                                    src="${productos[j].imagen2}"
-                                                    alt="Product Image 2">
-                                            </a>
-                                        </div>
-                                        <div class="col">
-                                            <a href="#">
-                                                <img
-                                                    class="card-img img-fluid shadow border rounded"
-                                                    style="height: 120px;"
-                                                    src="${productos[j].imagen3}"
-                                                    alt="Product Image 3">
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-1 align-self-center">
-                            <a href="#multi-item-example" role="button"
-                                data-bs-slide="next">
-                                <i class="text-dark fas fa-chevron-right"></i>
-                                <span class="sr-only">Next</span>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-7 mt-2">
-                    <div class="card">
-                        <div class="card-body shadow rounded p-4">
-                            <h1 class="h2">${productos[j].nombre}</h1>
-                            <p class="h3 py-2">${productos[j].precio}</p>
-                            <h6>Descripción:</h6>
-                            <p class="text-justify mt-2 mb-4 ">${productos[j].descripcion2}</p>
-
-                            <div class="row">
-                                <div class="col-auto">
-                                    <div class="d-inline-block pb-3">
-                                        <div
-                                            class="d-inline-block mr-3 mb-3">Tallas:
-                                            <input type="hidden"
-                                                name="product-size"
-                                                id="product-size"
-                                                value="S">
-                                        </div>
-                                        <div class="d-inline-block">
-                                            <span
-                                                class="btn btn-primary btn-size text-white m-1" id="talla36">36</span>
-                                            <span
-                                                class="btn btn-primary btn-size text-white m-1" id="talla37">37</span>
-                                            <span
-                                                class="btn btn-primary btn-size text-white m-1" id="talla38">38</span>
-                                            <span
-                                                class="btn btn-primary btn-size text-white m-1" id="talla39">39</span>
-                                            <span
-                                                class="btn btn-primary btn-size text-white m-1" id="talla40">40</span>
-                                            <span
-                                                class="btn btn-primary btn-size text-white m-1" id="talla41">41</span>
-                                            <span
-                                                class="btn btn-primary btn-size text-white m-1" id="talla42">42</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div
-                                class="col-auto d-flex justify-content-center align-items-center mt-2 mb-2">
-                                <div>
-                                    <ul class="list-inline">
-                                        <li
-                                            class="list-inline-item text-right">
-                                            Cantidad:
-                                            <input type="hidden"
-                                                name="product-quanity"
-                                                id="product-quanity"
-                                                value="1">
-                                        </li>
-                                        <li class="list-inline-item"><span
-                                                class="btn btn-primary"
-                                                id="btn-minus">-</span></li>
-                                        <li class="list-inline-item"><span
-                                                class="badge bg-white text-dark"
-                                                id="var-value">1</span></li>
-                                        <li class="list-inline-item"><span
-                                                class="btn btn-primary"
-                                                id="btn-plus">+</span></li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col d-grid">
-                                    <button type="submit"
-                                        class="btn btn-primary btn-lg"
-                                        name="submit"
-                                        value="addtocard">Añade al
-                                        carrito<i
-                                            class="bi bi-cart3 ml-2"></i></button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <script>
-        'use strict';
-$(document).ready(function () {
-
-  // Accordion
-  var all_panels = $('.templatemo-accordion > li > ul').hide();
-
-  $('.templatemo-accordion > li > a').click(function () {
-    console.log('Hello world!');
-    var target = $(this).next();
-    if (!target.hasClass('active')) {
-      all_panels.removeClass('active').slideUp();
-      target.addClass('active').slideDown();
-    }
-    return false;
-  });
-  // End accordion
-
-  // Product detail
-  $('.product-links-wap a').click(function () {
-    var this_src = $(this).children('img').attr('src');
-    $('#product-detail').attr('src', this_src);
-    return false;
-  });
-  $('#btn-minus').click(function () {
-    var val = $("#var-value").html();
-    val = (val == '1') ? val : val - 1;
-    $("#var-value").html(val);
-    $("#product-quanity").val(val);
-    return false;
-  });
-  $('#btn-plus').click(function () {
-    var val = $("#var-value").html();
-    val++;
-    $("#var-value").html(val);
-    $("#product-quanity").val(val);
-    return false;
-  });
-
-  ///se selecionan tallas
-  $('.btn-size').click(function () {
-    var this_val = $(this).html();
-    $("#product-size").val(this_val);
-    $(".btn-size").removeClass('btn-secondary');
-    $(".btn-size").addClass('btn-primary');
-    $(this).removeClass('btn-primary');
-    $(this).addClass('btn-secondary');
-    return false;
-  });
-  // End roduct detail
-
-});
-        </script>
-    `;
-    $('#cardDeportivoS').append(newTr2);
-
-    $('#talla36').hide();
-    $('#talla37').hide();
-    $('#talla38').hide();
-    $('#talla39').hide();
-    $('#talla40').hide();
-    $('#talla41').hide();
-    $('#talla42').hide();
-    if (productos[j].talla1 === '36' || productos[j].talla2 === '36' || productos[j].talla3 === '36') {
-        $('#talla36').show();
-    }
-    if (productos[j].talla1 === '37' || productos[j].talla2 === '37' || productos[j].talla3 === '37') {
-        $('#talla37').show();
-    }
-    if (productos[j].talla1 === '38' || productos[j].talla2 === '38' || productos[j].talla3 === '38') {
-        $('#talla38').show();
-    }
-    if (productos[j].talla1 === '39' || productos[j].talla2 === '39' || productos[j].talla3 === '39') {
-        $('#talla39').show();
-    }
-    if (productos[j].talla1 === '40' || productos[j].talla2 === '40' || productos[j].talla3 === '40') {
-        $('#talla40').show();
-    }
-    if (productos[j].talla1 === '41' || productos[j].talla2 === '41' || productos[j].talla3 === '41') {
-        $('#talla41').show();
-    }
-    if (productos[j].talla1 === '42' || productos[j].talla2 === '42' || productos[j].talla3 === '42') {
-        $('#talla42').show();
-    }
-}
 
--- js/navbarMostrar.js
+++ js/navbarMostrar.js
@@ -1,40 +1,62 @@
 document.addEventListener("DOMContentLoaded", function () {
     var navbar = document.createElement("div");
     navbar.innerHTML = `
-        <div class="container mt-3" style="z-index: 10; " id="dvContainerNV">
-            <nav class="navbar navbar-expand-lg rounded shadow" style="background-color: #e3f2fd; " id="nvBarraNV">
+         <div class="container mt-3" style="z-index: 10; " id="dvContainerNV">
+            <nav class="navbar navbar-expand-lg rounded shadow"
+                style="background-color: #e3f2fd; " id="nvBarraNV">
                 <div class="container-fluid bg-" id="dvFluidNV">
                     <!--Btn con img-->
                     <a class="navbar-brand" href="./index.html" id="aLogoNV">
-                        <img src="./img/Logo1.png" alt="Mundo del calzado" width="50" height="50" id="imgLogoNV">
+                        <img src="./img/Logo1.png" alt="Mundo del calzado"
+                            width="50" height="50" id="imgLogoNV">
                     </a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" id="btnToggleNV">
+                    <button class="navbar-toggler" type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation"
+                        id="btnToggleNV">
                         <!--Btn con Hamburguesa-->
-                        <span class="navbar-toggler-icon" id="spnIconoNV"></span>
+                        <span class="navbar-toggler-icon"
+                            id="spnIconoNV"></span>
                         <!--Btn con img-->
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent" id="dvColapsoNV">
-                        <div class="container navbar-collapse justify-content-center" id="dvContenedorInternoNV">
+                    <div class="collapse navbar-collapse"
+                        id="navbarSupportedContent" id="dvColapsoNV">
+                        <div
+                            class="container navbar-collapse justify-content-center"
+                            id="dvContenedorInternoNV">
                             <ul class="nav nav-pills" id="ulMenuNV">
                                 <li class="nav-item" id="liFormalNV">
-                                    <a class="nav-link" aria-current="page" href="#" id="aFormalNV">Formal</a>
+                                    <a class="nav-link" aria-current="page"
+                                        href="#" id="aFormalNV">Formal</a>
                                 </li>
                                 <li class="nav-item" id="liDeportivoNV">
-                                    <a class="nav-link" href="./HDeportivo.html" id="aDeportivoNV">Deportivo</a>
+                                    <a class="nav-link" href="./HDeportivo.html"
+                                        id="aDeportivoNV">Deportivo</a>
                                 </li>
                                 <li class="nav-item dropdown" id="liDropdownNV">
-                                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" id="aOtrosNV">
+                                    <a class="nav-link dropdown-toggle" href="#"
+                                        role="button" data-bs-toggle="dropdown"
+                                        aria-expanded="false" id="aOtrosNV">
                                         Otros
                                     </a>
-                                    <ul class="dropdown-menu" id="ulDropdownMenuNV">
-                                        <li><a class="dropdown-item" href="#" >Contáctanos</a></li>
-                                        <li><a class="dropdown-item" href="#" >¿Quiénes somos?</a></li>
+                                    <ul class="dropdown-menu"
+                                        id="ulDropdownMenuNV">
+                                        <li><a class="dropdown-item"
+                                                href="#">Contáctanos</a></li>
+                                        <li><a class="dropdown-item"
+                                                href="#">¿Quiénes
+                                                somos?</a></li>
                                     </ul>
                                 </li>
                             </ul>
                         </div>
-                        <div class="d-flex"  id="carritoNV">
-                            <button class="btn btn-outline-primary" type="submit" id="btnBuscarNV"><i class="bi bi-cart3" id="iIconoBuscarNV"></i></button>
+                        <div class="d-flex" id="carritoNV">
+                            <a href="#" class="btn btn-outline-primary d-flex"
+                                type="submit" id="btnBuscarNV"><i
+                                    class="bi bi-cart3"></i><span class="ml-2"
+                                    id="cuenta-carrito">0</span></a>
                         </div>
                     </div>
                 </div>
