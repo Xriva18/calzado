6f00c6c362583aabf8e34d222ca4ab07cd0dbc6f
Xriva18 <kevinguanoluisa2004@gmail.com>
2024-07-24 12:16:30 -0500

    base de datos zapatillas

--- js/zapatillas.js
+++ js/zapatillas.js
@@ -17,4 +17,22 @@ const zapatillas = [
         descripcion2: 'Disfruta de una comodidad y respuesta increíbles con estos tenis de running Ultraboost. Presentan una mediasuela BOOST que te proporciona un retorno de energía sin límites, un sistema Linear Energy Push y una suela con compuesto de caucho Continental™. Estos tenis presentan una parte superior fabricada con un hilo reciclado de alto rendimiento creado con al menos un 50% de Parley Ocean Plastic —  un material reinventado a partir de residuos plásticos recogidos en zonas costeras para evitar que contaminen nuestros océanos.',
         talla1: 38, talla2: 37, talla3: 40, precio: 169.99, imagen1: './img/Deportivo/depImg3.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
     },
+    {
+        id: 'Z004', nombre: 'Caven 2.0 Puma 39229003',
+        descripcion: 'Es momento de darle un sutil giro a tu guardarropa con el nuevo par de tenis Caven 2.0 para hombre de la marca Puma. ',
+        descripcion2: 'Es momento de darle un sutil giro a tu guardarropa con el nuevo par de tenis Caven 2.0 para hombre de la marca Puma. Esta silueta nacida en la década de 1980 te otorga una apariencia clásica con toques retro. Su diseño cuenta con la comodidad de Feelgood SoftFoam. Su construcción cuenta con al menos un 20% de contenido reciclado. En el costado se aprecia la característica franja de la marca Puma, su color se mezclará de manera armoniosa con cada uno de tus conjuntos. La plantilla SoftFoam proporciona comodidad durante todo el día. Cordones para un buen ajuste. Material superior: cuero, cuero sintético. Entresuela de goma. Suela de goma.',
+        talla1: 35, talla2: 36, talla3: 38, precio: 95.39, imagen1: './img/Deportivo/depImg4.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
+    },
+    {
+        id: 'Z005', nombre: 'GAZELLE REAL MADRID',
+        descripcion: 'Tu amor por el fútbol corre por tus venas y por estos tenis adidas Gazelle.',
+        descripcion2: 'Tu amor por el fútbol corre por tus venas y por estos tenis adidas Gazelle. Estos tenis con las 3 Rayas vuelven a sus raíces deportivas y se engalanan con los colores y el logotipo que demuestran apoyo a tu club. El toque final lo ponen el exterior de gamuza y su famoso diseño de corte bajo.',
+        talla1: 38, talla2: 37, talla3: 40, precio: 169.99, imagen1: './img/Deportivo/depImg5.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
+    },
+    {
+        id: 'Z006', nombre: 'Air Max Plus x FC Barcelona x Patta',
+        descripcion: 'Luce los colores del Barça con este modelo. Diseñadas en colaboración con la icónica marca holandesa Patta. ',
+        descripcion2: 'Luce los colores del Barça con este modelo. Diseñadas en colaboración con la icónica marca holandesa Patta, estas Air Max Plus mantienen la larga tradición de colaboraciones entre el FC Barcelona y los visionarios neerlandeses. La parte superior y la placa del mediopié están cubiertas con un motivo degradado fluido con los colores deep royal blue y noble red para que representes al equipo azulgrana. Las chapas de edición especial cuentan con el escudo del Barça y las puntas de los cordones recuerdan a las rayas de la bandera catalana. El look se completa con elegantes líneas de diseño que aportan la estética inconfundible de las Air Max Plus, otro recordatorio destacado del legado de la grandeza.',
+        talla1: 40, talla2: 41, talla3: 42, precio: 169.99, imagen1: './img/Deportivo/depImg6.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
+    },
 ];
