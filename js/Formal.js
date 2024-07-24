c62330d3388fb49d372f9a71e895b9f974cdd9af
CRlSSlS <133447314+CRlSSlS@users.noreply.github.com>
2024-07-24 09:13:39 -0500

    agregue formal

---
+++ js/Formal.js
@@ -0,0 +1,20 @@
+const zapatillas = [
+    {
+        id: 'F001', nombre: 'PUMA California Tol',
+        descripcion: 'Vas a estar cómodo dentro y fuera de la cancha con estas zapatillas retro, inspiradas en la cultura pop clásica y en el estilo de baloncesto.',
+        descripcion2: 'Uno de los zapatos de entrenamiento casuales más llamativos de la línea PUMA Tennis, el PUMA California es un O.G. Estilo nacido en las canchas en 1983 del campeón argentino de Grand Slam Guillermo Vilas. Pronto llegó a las calles de California, donde adoptó su nuevo nombre y quedó consagrado para siempre en la historia de las zapatillas. Con la misma silueta clásica de California, esta versión presenta una parte superior perforada con un diseño de bloques de color único y el logotipo de PUMA.',
+        talla1: 36, talla2: 37, talla3: 38, precio: 109.99, imagen1: './img/Deportivo/depImg1.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
+    },
+    {
+        id: 'Z002', nombre: 'Nike Air Max 270',
+        descripcion: 'Estas zapatillas ofrecen una comodidad excepcional y un estilo moderno con su unidad Max Air en el talón y su diseño inspirado en los años 90.',
+        descripcion2: 'Los Nike Air Max 270, los primeros Air Max para el día a día de Nike, te aportan estilo, comodidad y gran actitud. El diseño se inspira en los íconos clásicos de Air Max y muestra la innovación más grande de Nike con su gran ventana y su nueva variedad de colores.',
+        talla1: 39, talla2: 40, talla3: 42, precio: 149.99, imagen1: './img/Deportivo/depImg2.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
+    },
+    {
+        id: 'Z003', nombre: 'Adidas Ultraboost',
+        descripcion: 'Estas zapatillas de running te brindan una increíble amortiguación y retorno de energía para que puedas correr más rápido y por más tiempo.',
+        descripcion2: 'Disfruta de una comodidad y respuesta increíbles con estos tenis de running Ultraboost. Presentan una mediasuela BOOST que te proporciona un retorno de energía sin límites, un sistema Linear Energy Push y una suela con compuesto de caucho Continental™. Estos tenis presentan una parte superior fabricada con un hilo reciclado de alto rendimiento creado con al menos un 50% de Parley Ocean Plastic —  un material reinventado a partir de residuos plásticos recogidos en zonas costeras para evitar que contaminen nuestros océanos.',
+        talla1: 38, talla2: 37, talla3: 40, precio: 169.99, imagen1: './img/Deportivo/depImg3.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
+    },
+];
