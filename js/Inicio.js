bcc70336ad4668db77ea1e1d7cdafc529dd0d3e9
CRlSSlS <133447314+CRlSSlS@users.noreply.github.com>
2024-07-24 17:21:47 -0500

    calsado Inicio

--- js/Inicio.js
+++ js/Inicio.js
@@ -1,39 +1,39 @@
 const Inicios = [
     {
-        id: 'I001', nombre: '',
-        descripcion: '',
-        descripcion2: '',
-        talla1: 38, talla2: 39, talla3: 40, precio: 42.99, imagen1: './img/Formal/forImg1.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
+        id: 'I001', nombre: 'Nike MC Trainer 2',
+        descripcion: 'Nike MC Trainer 2 son zapatos de entrenar para hombre, que te ayudarán a completar tus circuitos en el gimnasio, al aire libre o donde realices tus rutinas',
+        descripcion2: 'Nike MC Trainer 2 son zapatos de entrenar para hombre, que te ayudarán a completar tus circuitos en el gimnasio, al aire libre o donde realices tus rutinas. La base plana es más ancha alrededor del talón para ayudar a estabilizarte en trabajos como pesas, incorpora superposiciones alrededor de la puntera, y los cordones son fáciles de ajustar para que cumplas con tus objetivos,Beneficios del producto: Baseplana es más ancha alrededor del talón, Sujeción envuelve desde el talón hasta los laterales, Banda de rodadura de goma te proporciona tracción desde el césped hasta el suelo de la sala de entrenamiento, Malla en la parte superior permite que tu pie respire, Tracción de goma direccional en el lateral te da agarre en las cuerdas.',
+        talla1: 40, talla2: 42, talla3: 44, precio: 199.99, imagen1: './img/Inicio/inicImg1.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
     },
     {
-        id: 'I002', nombre: '',
-        descripcion: '',
-        descripcion2: '',
-        talla1: 38, talla2: 39, talla3: 40, precio: 42.99, imagen1: './img/Formal/forImg1.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
+        id: 'I002', nombre: 'Under Armour HOVR Turbulence 2',
+        descripcion: 'Estos zapatos, diseñados específicamente para corredores de larga distancia, ofrecen una combinación perfecta de amortiguación, rebote, durabilidad y eficiencia.',
+        descripcion2: 'Estos zapatos, diseñados específicamente para corredores de larga distancia, ofrecen una combinación perfecta de amortiguación, rebote, durabilidad y eficiencia. La tecnología UA HOVR proporciona una amortiguación reactiva que reduce el impacto y devuelve la energía, impulsándote hacia adelante. Su parte superior de punto transpirable garantiza una contención ligera y duradera, ideal para entrenamientos diarios intensivos, Beneficios del producto: Amortiguación reactiva UA HOVR para reducir el impacto, Parte superior de punto transpirable para mayor durabilidad, Plantilla con sistema de confort de lujo, Suela de goma duradera con flexibilidad para el despegue, Ideal para entrenamientos diarios deportivos, Excelente retorno de energía en cada pisada, Diseño que favorece una pisada suave, Proporciona comodidad durante largas distancias, Optimiza la eficiencia del corredor, Ajuste regular para adaptarse a diversos tipos de pie',
+        talla1: 38, talla2: 39, talla3: 40, precio: 129.99, imagen1: './img/Inicio/inicImg2.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
     },
     {
-        id: 'I003', nombre: '',
-        descripcion: '',
-        descripcion2: '',
-        talla1: 38, talla2: 39, talla3: 40, precio: 42.99, imagen1: './img/Formal/forImg1.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
+        id: 'I003', nombre: 'Reebok FloatZig 1',
+        descripcion: 'Los zapatos para correr, Reebok FloatZig 1 te brindan alto retorno de energía. Diseñados para inspirar a todos a ponerse los cordones y salir al aire libre.',
+        descripcion2: 'Los zapatos para correr, Reebok FloatZig 1 te brindan alto retorno de energía. Diseñados para inspirar a todos a ponerse los cordones y salir al aire libre, están construidas con tecnología de espuma liviana de primera calidad en una geometría que mejora la amortiguación, el retorno de energía y una transición suave. para brindar una experiencia única bajo los pies, Beneficios del producto: Parte superior de malla diseñada con superposiciones reflectantes, Ajuste regular, Cierre de encaje, Espuma energética Floatride, Suela de goma, Caída de la entresuela: 6 mm',
+        talla1: 36, talla2: 38, talla3: 39, precio: 169.99, imagen1: './img/Inicio/inicImg3.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
     },
     {
-        id: 'I004', nombre: '',
-        descripcion: '',
-        descripcion2: '',
-        talla1: 38, talla2: 39, talla3: 40, precio: 42.99, imagen1: './img/Formal/forImg1.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
+        id: 'I004', nombre: 'Zapato Formal Derby con Pasador Dauss',
+        descripcion: 'Viste un look sofisticado con este modelo de zapato formal de la marca Dauss. Una propuesta elaborada en cuero, ideal para brindarte comodidad y un estilo único en tus eventos sociales por la noche.',
+        descripcion2: 'Viste un look sofisticado con este modelo de zapato formal de la marca Dauss. Una propuesta elaborada en cuero, ideal para brindarte comodidad y un estilo único en tus eventos sociales por la noche. Zapato Formal Dauss, Diseño unicolor, Modelo derby, Ajuste de pasador, Elaborada en cuero',
+        talla1: 38, talla2: 39, talla3: 40, precio: 64.59, imagen1: './img/Inicio/inicImg4.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
     },
     {
-        id: 'I005', nombre: '',
-        descripcion: '',
-        descripcion2: '',
-        talla1: 38, talla2: 39, talla3: 40, precio: 42.99, imagen1: './img/Formal/forImg1.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
+        id: 'I005', nombre: 'Zapato Formal Derby con Pasador Pegada',
+        descripcion: 'Agrega a tu colección de calzado este modelo de zapato derby, unas piezas en diseño combinado y ajuste con pasador, perfecta para tus atuendos formales en estas fiestas.',
+        descripcion2: 'Agrega a tu colección de calzado este modelo de zapato derby, unas piezas en diseño combinado y ajuste con pasador, perfecta para tus atuendos formales en estas fiestas. Zapato Formal Pegada, Diseño combinado, Modelo derby, Ajuste con pasador, Elaborado en cuero',
+        talla1: 40, talla2: 42, talla3: 43, precio: 83.13, imagen1: './img/Inicio/inicImg5.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
     },
     {
-        id: 'I006', nombre: '',
-        descripcion: '',
-        descripcion2: '',
-        talla1: 38, talla2: 39, talla3: 40, precio: 42.99, imagen1: './img/Formal/forImg1.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
+        id: 'I006', nombre: 'Tacón Cuadrado Bebece',
+        descripcion: 'Los tacones geométricos por su forma te ofrecen comodidad y estabilidad al caminar, este modelo de Bebece tiene una silueta en punta que estiliza tus looks.',
+        descripcion2: 'Los tacones geométricos por su forma te ofrecen comodidad y estabilidad al caminar, este modelo de Bebece tiene una silueta en punta que estiliza tus looks. Un par que sin duda podrás combinar con atuendos formales o ejecutivos. Tacón Bebece, Diseño unicolor, Taco cuadrado, Ajuste con correa, Alto 5.5 cm, Elaborado en sintético ',
+        talla1: 35, talla2: 36, talla3: 37, precio: 51.32, imagen1: './img/Inicio/inicImg6.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
     },
 
 ];
