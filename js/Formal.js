7a77e237ec53528af28111a6ad00fb2f21286a02
CRlSSlS <133447314+CRlSSlS@users.noreply.github.com>
2024-07-24 12:14:37 -0500

    ingreso zapatos formales

--- js/Formal.js
+++ js/Formal.js
@@ -1,20 +1,39 @@
-const zapatillas = [
+const Formales = [
     {
-        id: 'F001', nombre: 'PUMA California Tol',
-        descripcion: 'Vas a estar cómodo dentro y fuera de la cancha con estas zapatillas retro, inspiradas en la cultura pop clásica y en el estilo de baloncesto.',
-        descripcion2: 'Uno de los zapatos de entrenamiento casuales más llamativos de la línea PUMA Tennis, el PUMA California es un O.G. Estilo nacido en las canchas en 1983 del campeón argentino de Grand Slam Guillermo Vilas. Pronto llegó a las calles de California, donde adoptó su nuevo nombre y quedó consagrado para siempre en la historia de las zapatillas. Con la misma silueta clásica de California, esta versión presenta una parte superior perforada con un diseño de bloques de color único y el logotipo de PUMA.',
-        talla1: 36, talla2: 37, talla3: 38, precio: 109.99, imagen1: './img/Deportivo/depImg1.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
+        id: 'F001', nombre: 'Zapato Formal Derby Expressions Basic',
+        descripcion: 'Camina como todo un caballero junto con este modelo de zapato formal de la marca Expressions Basic, un diseño clásico y cómodo.',
+        descripcion2: 'Camina como todo un caballero junto con este modelo de zapato formal de la marca Expressions Basic, un diseño clásico y cómodo, perfecto para combinar con tus pantalones y camisas manga largas. Zapato Formal Expressions Basic, Diseño unicolor, Modelo derby con pasador,Elaborado en sintético',
+        talla1: 38, talla2: 39, talla3: 40, precio: 42.99, imagen1: './img/Formal/forImg1.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
     },
     {
-        id: 'Z002', nombre: 'Nike Air Max 270',
-        descripcion: 'Estas zapatillas ofrecen una comodidad excepcional y un estilo moderno con su unidad Max Air en el talón y su diseño inspirado en los años 90.',
-        descripcion2: 'Los Nike Air Max 270, los primeros Air Max para el día a día de Nike, te aportan estilo, comodidad y gran actitud. El diseño se inspira en los íconos clásicos de Air Max y muestra la innovación más grande de Nike con su gran ventana y su nueva variedad de colores.',
-        talla1: 39, talla2: 40, talla3: 42, precio: 149.99, imagen1: './img/Deportivo/depImg2.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
+        id: 'F002', nombre: 'Zapato Formal Derby de Cuero Pegada',
+        descripcion: 'Los zapatos formales de Pegada están diseñados para ofrecerte un estilo impecable y un confort incomparable.',
+        descripcion2: 'Este modelo elaborado en cuero tiene una estética elegante que resaltará tus atuendos. Zapato Formal Pegada, Diseño combinado , Modelo Derby , Ajuste con pasador , Elaborado en cuero ',
+        talla1: 39, talla2: 40, talla3: 42, precio: 92.40, imagen1: './img/Formal/forImg2.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
     },
     {
-        id: 'Z003', nombre: 'Adidas Ultraboost',
-        descripcion: 'Estas zapatillas de running te brindan una increíble amortiguación y retorno de energía para que puedas correr más rápido y por más tiempo.',
-        descripcion2: 'Disfruta de una comodidad y respuesta increíbles con estos tenis de running Ultraboost. Presentan una mediasuela BOOST que te proporciona un retorno de energía sin límites, un sistema Linear Energy Push y una suela con compuesto de caucho Continental™. Estos tenis presentan una parte superior fabricada con un hilo reciclado de alto rendimiento creado con al menos un 50% de Parley Ocean Plastic —  un material reinventado a partir de residuos plásticos recogidos en zonas costeras para evitar que contaminen nuestros océanos.',
-        talla1: 38, talla2: 37, talla3: 40, precio: 169.99, imagen1: './img/Deportivo/depImg3.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
+        id: 'F003', nombre: 'Zapato Formal Derby con Pasador Democrata',
+        descripcion: 'La marca Democrata trae para el caballero moderno este modelo de zapato formal.',
+        descripcion2: 'La marca Democrata trae para el caballero moderno este modelo de zapato formal, un diseño elegante, perfecto para combinar con tus pantalones y camisas. Zapato Formal Democrata, Diseño unicolor, Modelo derby con pasador, Elaborado en sintético',
+        talla1: 36, talla2: 37, talla3: 38, precio: 50.30, imagen1: './img/Formal/forImg3.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
     },
+    {
+        id: 'F004', nombre: 'Zapato Formal Derby con Pasador Democrata ',
+        descripcion: 'Un esencial dentro del armario de todo caballero. Elige el nuevo modelo de zapato formal de la marca Democrata.',
+        descripcion2: 'Un esencial dentro del armario de todo caballero. Elige el nuevo modelo de zapato formal de la marca Democrata, una propuesta perfecta para combinar con tus diferentes atuendos casuales o semi formales. Zapato Formal Democrata, Diseño unicolor, Modelo derby, Ajuste de pasador, Elaborado en sintético ',
+        talla1: 36, talla2: 37, talla3: 38, precio: 71.86, imagen1: './img/Formal/forImg4.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
+    },
+    {
+        id: 'F005', nombre: 'Tacón Fino con Hebilla Bebece',
+        descripcion: 'Los detalles de hebilla en los tacones le dan estructura y carácter a tu estilo.',
+        descripcion2: 'Los detalles de hebilla en los tacones le dan estructura y carácter a tu estilo. Bebece te trae este modelo con un cautivador tacón fino y la altura ideal para lucir en tus próximos eventos navideños. Tacón Bebece, Diseño unicolor, Modelo con hebilla en punta, Tipo de taco: fino, Alto de taco: 6.5 cm, Elaborado 100% sintético',
+        talla1: 32, talla2: 34, talla3: 36, precio: 75.59, imagen1: './img/Formal/forImg5.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
+    },
+    {
+        id: 'F006', nombre: 'Tacón Fino Metalizado Vizzano',
+        descripcion: 'Enamórate de cada paso que des en tu boda junto con los nuevos tacones de Vizzano.',
+        descripcion2: 'namórate de cada paso que des en tu boda junto con los nuevos tacones de Vizzano. Este par metalizado con una elegante correa en el tobillo será el match perfecto para lucir radiante con tu vestido de novia. Tacón Vizzano, Diseño metalizado, Ajuste con correa en el tobillo, Taco fino , Alto 9.5 cm , Elaborado en sintético ',
+        talla1: 32, talla2: 34, talla3: 36, precio: 61.99, imagen1: './img/Formal/forImg6.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
+    },
+
 ];
---
+++ js/Inicio.js
@@ -0,0 +1,39 @@
+const Inicios = [
+    {
+        id: 'I001', nombre: '',
+        descripcion: '',
+        descripcion2: '',
+        talla1: 38, talla2: 39, talla3: 40, precio: 42.99, imagen1: './img/Formal/forImg1.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
+    },
+    {
+        id: 'I002', nombre: '',
+        descripcion: '',
+        descripcion2: '',
+        talla1: 38, talla2: 39, talla3: 40, precio: 42.99, imagen1: './img/Formal/forImg1.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
+    },
+    {
+        id: 'I003', nombre: '',
+        descripcion: '',
+        descripcion2: '',
+        talla1: 38, talla2: 39, talla3: 40, precio: 42.99, imagen1: './img/Formal/forImg1.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
+    },
+    {
+        id: 'I004', nombre: '',
+        descripcion: '',
+        descripcion2: '',
+        talla1: 38, talla2: 39, talla3: 40, precio: 42.99, imagen1: './img/Formal/forImg1.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
+    },
+    {
+        id: 'I005', nombre: '',
+        descripcion: '',
+        descripcion2: '',
+        talla1: 38, talla2: 39, talla3: 40, precio: 42.99, imagen1: './img/Formal/forImg1.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
+    },
+    {
+        id: 'I006', nombre: '',
+        descripcion: '',
+        descripcion2: '',
+        talla1: 38, talla2: 39, talla3: 40, precio: 42.99, imagen1: './img/Formal/forImg1.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
+    },
+
+];
