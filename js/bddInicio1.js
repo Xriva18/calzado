31badbdcb1168151caa8c06e07152223ec76c45b
Xriva18 <kevinguanoluisa2004@gmail.com>
2024-07-25 20:25:45 -0500

    diseño final

--- js/bddInicio1.js
+++ js/bddInicio1.js
@@ -14,7 +14,7 @@ const Inicios = [
     },
     {
         id: 'I003', nombre: 'Tenis Samba OG',
-        descripcion: 'Nacidos en las canchas de fútbol, los Samba son un ícono atemporal de la moda urbana. ',
+        descripcion: 'Nacidos en las canchas de fútbol, los Samba son un ícono atemporal de la moda urbana.Esta silueta se mantiene fiel a su legado con un discreto y elegante exterior de cuero suave. ',
         descripcion2: 'Nacidos en las canchas de fútbol, los Samba son un ícono atemporal de la moda urbana. Esta silueta se mantiene fiel a su legado con un discreto y elegante exterior de cuero suave, revestimientos de gamuza y una suela de caucho natural, convirtiéndose en un par indispensable para cualquier armario.',
         talla1: 35, talla2: 36, talla3: 37, precio: 51.32, imagen1: './img/Inicio/inicImg3.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
     },
@@ -25,15 +25,15 @@ const Inicios = [
         talla1: 38, talla2: 39, talla3: 40, precio: 64.59, imagen1: './img/Inicio/inicImg4.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
     },
     {
-        id: 'I005', nombre: '',
-        descripcion: '',
-        descripcion2: '',
+        id: 'I005', nombre: 'Nike G.T. Hustle 3 Electric',
+        descripcion: 'Las G.T. Hustle 3 te ayudan a superar cualquier dificultad. Con una amortiguación Air Zoom doble que proporciona una potencia reactiva, te permiten ahorrar energía durante el transcurso del partido. ',
+        descripcion2: 'Las G.T. Hustle 3 te ayudan a superar cualquier dificultad. Con una amortiguación Air Zoom doble que proporciona una potencia reactiva, te permiten ahorrar energía durante el transcurso del partido. Está diseñada para quienes quieren superar a sus oponentes y mantener la energía hasta el final. ¿Quién se apunta? ¡Adelante! Cuanto más reactivas sean las zapatillas, mayor retorno de energía podrás obtener en cada pisada. Las zapatillas reactivas ofrecen un poco más de elasticidad en cada pisada para sacar el máximo partido a tus movimientos. La unidad Air Zoom del antepié tiene forma de herradura. Esto te permite experimentar toda la reactividad de la unidad Air Zoom en los laterales y los metatarsos. Cuanta más amortiguación haya en la planta del pie, más cómoda será la experiencia de jugar. La unidad Air Zoom en la capa inferior de la parte superior se combina con una pila más alta de espuma suave y elástica para ofrecer una transición suave del talón a la puntera y proporcionar comodidad en la planta del pie.',
         talla1: 40, talla2: 42, talla3: 43, precio: 83.13, imagen1: './img/Inicio/inicImg5.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
     },
     {
-        id: 'I006', nombre: '',
-        descripcion: '',
-        descripcion2: '',
+        id: 'I006', nombre: 'Nike Alphafly 3',
+        descripcion: 'Perfeccionados para la velocidad de un maratón, los Alphafly 3 te ayudan a ir más allá de lo que creías posible. Tres tecnologías innovadoras impulsan tu carrera',
+        descripcion2: 'Perfeccionados para la velocidad de un maratón, los Alphafly 3 te ayudan a ir más allá de lo que creías posible. Tres tecnologías innovadoras impulsan tu carrera: una dosis doble de unidades Air Zoom te ayuda a lanzarte a tu siguiente etapa; una placa de fibra de carbono de largo completo te ayuda a impulsarte hacia adelante con facilidad; y una entresuela de espuma ZoomX Foam del talón a la punta te ayuda a mantener la frescura desde la largada hasta el kilómetro 42.',
         talla1: 35, talla2: 36, talla3: 37, precio: 51.32, imagen1: './img/Inicio/inicImg6.jpg', imagen2: './img/Vista1.jpg', imagen3: './img/Vista2.jpg'
     },
 
