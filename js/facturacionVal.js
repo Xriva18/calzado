e0622c8dec2a79f0385fdca3c0f3a453f8ff04e0
Xriva18 <kevinguanoluisa2004@gmail.com>
2024-07-26 00:07:52 -0500

    pdf encabezado

--- js/facturacionVal.js
+++ js/facturacionVal.js
@@ -2,6 +2,7 @@ const inputFile = document.querySelector('#input-foto');
 const image = document.querySelector('#previa');
 const tarjetaDiv = document.querySelector('#tarjetaDiv');
 const depositoDiv = document.querySelector('#depositoDiv');
+let FotoMostrar;
 
 function encodeFileAsBase64URL(file) {
     return new Promise((resolve) => {
@@ -15,8 +16,8 @@ function encodeFileAsBase64URL(file) {
 
 inputFile.addEventListener('input', async (event) => {
     if (inputFile.files.length > 0) {
-        const base64URL = await encodeFileAsBase64URL(inputFile.files[0]);
-        image.setAttribute('src', base64URL);
+        FotoMostrar = await encodeFileAsBase64URL(inputFile.files[0]);  // Almacena en FotoMostrar
+        image.setAttribute('src', FotoMostrar);
         image.classList.remove('d-none');
     }
 });
@@ -79,6 +80,79 @@ document.querySelectorAll('input[name="reb_pago"]').forEach(radio => {
         return true;
     });
 }*/
-function GenerarPDF() {
 
+///onclick="if (validarFormularioFact()) { GenerarPDF(); alert('Su pago se realizó con éxito'); }
+function GenerarPDF() {
+    var doc = new jsPDF();
+    var y = 25;
+    var x = 20;
+    //encabezado
+    doc.setFont('HELVETICA');
+    doc.setFontStyle('bold');
+    doc.setFontSize(18);
+    doc.text(87, 15, 'Facturación');
+    //datos titulos
+    doc.setFontSize(12);
+    doc.setFontStyle('times');
+    doc.setFontStyle('bold');
+    doc.text(x, y, 'Nombre:');
+    y += 5;
+    doc.text(x, y, 'Cedula: ');
+    y += 5;
+    doc.text(x, y, 'Correo: ');
+    y += 5;
+    if ($('#rbTarjeta').is(':checked')) {
+        doc.text(x, y, 'Pago por Tarjeta ');
+        y += 5;
+        doc.text(x, y, 'Nombre de la tarjeta: ');
+        y += 5;
+        doc.text(x, y, '# Tarjeta: ');
+        y += 5;
+        doc.text(x, y, 'CVC: ');
+        y += 5;
+        doc.text(x, y, 'Vencimiento: ');
+        y += 5;
+    }
+    if ($('#rbDeposito').is(':checked')) {
+        doc.text(x, y, 'Pago por Transferencia');
+        y += 5;
+        doc.text(x, y, '#Comprobante: ');
+        y += 5;
+    }
+    doc.text(x, y, 'Total a pagar: ');
+
+    //datos relleno
+    x = 50;
+    y = 25;
+    doc.setFontStyle('normal');
+    doc.text(x, y, $('#txtNombreCliente').val());
+    y += 5;
+    doc.text(x, y, $('#txtCedula').val());
+    y += 5;
+    doc.text(x, y, $('#txtCorreo').val());
+    y += 5;
+    if ($('#rbTarjeta').is(':checked')) {
+        y += 5;
+        doc.text(x + 12, y, $('#txtNombreTarjeta').val());
+        y += 5;
+        doc.text(x + 12, y, $('#txtNumeroTarjeta').val());
+        y += 5;
+        doc.text(x + 12, y, $('#txtCVC').val());
+        y += 5;
+        doc.text(x + 12, y, $('#txtFechaVencimiento').val());
+        y += 5;
+    }
+    if ($('#rbDeposito').is(':checked')) {
+        y += 5;
+        doc.text(x, y, $('#txtNumeroComprobante').val());
+        y += 5;
+        doc.addImage(FotoMostrar, 'JPEG', 120, 20, 25, 25);
+        y += 5;
+    }
+    doc.text(x, y, $('#precio-total').text());
+
+
+    ///impresion de la factura
+    var nombreArchivo = 'Factura_0.pdf';
+    doc.save(nombreArchivo);
 }
