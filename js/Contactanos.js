2d1bf24b5621e648b3a12376946466cdc73486b6
Xriva18 <kevinguanoluisa2004@gmail.com>
2024-07-25 22:11:06 -0500

    valdiaciones de contactanos correcto

--- js/Contactanos.js
+++ js/Contactanos.js
@@ -11,6 +11,30 @@ $("#ddlRazonCon").change(function () {
 });
 
 function validacionContacto() {
+    if ($("#txtNombreCon").val() == "") {
+        swal("Error", "El campo Nombre es obligatorio", "error");
+        return false;
+    }
+    if ($("#txtCedulaCon").val() == "") {
+        swal("Error", "El campo Cedula es obligatorio", "error");
+        return false;
+    }
+    if ($("#txtCorreoCon").val() == "") {
+        swal("Error", "El campo Correo es obligatorio", "error");
+        return false;
+    }
+    if ($("#txtTelefonoCon").val() == "") {
+        swal("Error", "El campo Telefono es obligatorio", "error");
+        return false;
+    }
+    if ($("#ddlRazonCon").val() == "0") {
+        swal("Error", "El campo Razon es obligatorio", "error");
+        return false;
+    }
+    if ($("#ddlRazonCon").val() == "3" && $("#txtOtrosCon").val() == "") {
+        swal("Error", "El campo Observacion es obligatorio", "error");
+        return false;
+    }
     swal("Enviado", "Pronto nos pondremos en contacto contigo!", "success");
 
 };
