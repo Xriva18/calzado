52a8f7897b8e4ada80d3495c39e7a49e64814942
Xriva18 <kevinguanoluisa2004@gmail.com>
2024-08-28 23:16:26 -0500

    conexion a la base de datos

---
+++ bdd/database.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'calazado_dbb'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err.stack);
+        return;
+    }
+    console.log('Connected to the database as id ' + connection.threadId);
+});
+module.exports = connection;
